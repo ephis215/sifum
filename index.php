1da27c2d4f064447227018ffa759b06eda12bd21
ephis215 <75049400+ephis215@users.noreply.github.com>
2021-07-12 20:17:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inventario Unicla</title>
+	<?php include("./lib/01header.php");
+
+	?>
+
+</head>
+
+<header>
+	
+	<?php include("./lib/02menu.php"); ?>
+
+</header>
+
+<body>
+	<br>
+	<br>
+
+
+	<img src="img/inventario.jpg" width="100%">
+
+	<br>
+
+	<div class="row">
+		<div class="col-sm-4">
+        <h1 class="text-center">Mision</h1>
+
+        <h3 class="text-center">La UNICLA se consolidará en la docencia, investigación y difusión de la cultura, optimizando continuamente sus procesos de enseñanza-aprendizaje y forjando una relación de excelencia entre estudiantes, docentes y administrativos.</h3>
+		</div>
+
+		<div class="col-sm-4">
+			<h1 class="text-center">Vision</h1>
+			<h3>Ser una Institución Educativa socialmente responsable que sea reconocida por su calidad académica y calidez humana, innovando la gama e incrementando la cobertura a nivel global de programas académicos en los niveles medio superior y superior. La UNICLA se consolidará en la docencia.</h3>
+		</div>
+
+		<div class="col-sm-4">
+			<h1 class="text-center">Valores</h1>
+			<h3 class="text-center">La Universidad Contemporánea de las Américas educa en un entorno solidario hacia la comunidad, sin distinción social, religiosa y política, siempre basándose en las leyes de convivencia universal como: el respeto, la tolerancia, la disciplina y la libertad sin perder de vista la importancia de la ética profesional.</h3>
+			
+		</div>
+
+	</div>
+
+</body>
+
+
+
+
+<footer>
+	<?php include("./lib/04footer.php");  ?>
+</footer>
+</html>
